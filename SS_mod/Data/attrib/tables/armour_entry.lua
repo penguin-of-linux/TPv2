6f16e975b0269f04d5879bcb103b5cb5ea8f04f5
khapov <khapov@skbkontur.ru>
2021-07-13 21:21:14 +0500

    Добавил броню infantry_necron

---
+++ SS_mod/Data/attrib/tables/armour_entry.lua
@@ -0,0 +1,4 @@
+GameData = Inherit([[]])
+GameData["armour_piercing_value"] = 0
+GameData["armour_type"] = Reference([[type_armour\tp_armour.lua]])
+GameData["armour_type"]["screen_name_id"] = [[$90100]] -- Armour
---
+++ SS_mod/Data/attrib/type_armour/tp_infantry_heavy_high.lua
@@ -0,0 +1,2 @@
+GameData = Inherit([[]])
+GameData["screen_name_id"] = [[Elite Heavy Infantry]]
